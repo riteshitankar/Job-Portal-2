02a9f4e2cfd688420825634157dff166bd89cd31
riteshitankar <riteshitankar99@gmail.com>
2025-12-04 15:34:31 +0530

    level 2 hard reset required in the future all the data is in ass4

--- server/utils/redisClient.js
+++ server/utils/redisClient.js
@@ -1,28 +1,3 @@
-// import { createClient } from "redis"
-// import dotenv from "dotenv"
-
-// dotenv.config({ path: "./config.env" })
-
-// const redisClient = createClient({
-//     url: process.env.REDIS_URL
-// })
-
-// redisClient.on("error", (err) => {
-//     console.log("redis error : ", err)
-// })
-
-// try {
-//     await redisClient.connect();
-//     console.log("successfully conected to redis client !")
-// } catch (err) {
-//     console.log("unable to connect with redis client : ", err)
-// }
-
-// export { redisClient }
-
-// // to store a temp data(otp)
-
-// server/utils/redisClient.js
 import { createClient } from "redis";
 import dotenv from "dotenv";
 dotenv.config({ path: "./config.env" });
